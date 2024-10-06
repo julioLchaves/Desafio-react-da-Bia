89c70cc8cbf733dcb5b4e46a24f76789d8e82242
julioLchaves <juliodelucasneto@outlook.com>
2024-10-06 20:47:30 -0300

    Função para completar lista

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Todo = ({ todo , removeTodo}) => {
+const Todo = ({ todo , removeTodo, completeTodo}) => {
   return (
-    <div className="todo" key={todo.id}>
+    <div className="todo" style={{textDecoration:todo.isCompleted? "line-through":""}} key={todo.id}>
       <div className="content">
         <p>{todo.text}</p>
         <div className="category">({todo.category})</div>
       </div>
       <div>
-        <button className='complete'>Completar</button>
+        <button className='complete'onClick={()=>completeTodo(todo.id)} ></button>
         <button className='remove'onClick={()=>removeTodo(todo.id)}>Deletar</button>
       </div>
     </div>
