30dfa1f9c296170f03f3a2f0037987e49fbca5cf
julioLchaves <juliodelucasneto@outlook.com>
2024-10-06 19:39:03 -0300

    Atualizando o formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
+import './App.css';
+import Todo from './components/Todo';
+import TodoForm from './components/TodoForm';
 
 function App() {
-  const [todos, setTodos]= useState([
+  const [todos, setTodos] = useState([
     {
-      id:1,
+      id: 1,
       text: "Criar funcionalidade x no sistema",
       category: "Trabalho",
-      isCompleted:false,
+      isCompleted: false,
     },
     {
-      id:2,
-      text:"Ir para academia",
-      category:"Pessoal",
-      isCompleted:false,
+      id: 2,
+      text: "Ir para academia",
+      category: "Pessoal",
+      isCompleted: false,
     },
     {
-      id:3,
+      id: 3,
       text: "Estudar React",
-      category:"Estudos",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ]);
+
+  const addTodo= (text, category)=> {
+    const newTodos=[...todos,{
+      id: Math.floor(Math.random()*10000),
+      text,
+      category,
       isCompleted:false,
-    }
-  ])
+    },
+  ];
+
+  setTodos(newTodos);
+  };
 
   return (
-
-    <div className='app'>
-      
+    <div className="app">
+      <h1>Lista de tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <Todo key={todo.id} todo={todo}/>
+        ))}
+      </div>
+      <TodoForm addTodo={addTodo}/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
